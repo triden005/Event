753f332f8490e72344c6813c0389d9b2d45dbb0c
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-11-19 02:10:01 +0530

    add styles for register page

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -7,116 +7,138 @@ import { AddAlert } from "../../_action/AlertAction";
 import { register } from "../../_action/AuthAction";
 
 class Register extends React.Component {
-    //form will have all the form data
-    state = {
-        form: {},
-    };
+  //form will have all the form data
+  state = {
+    form: {},
+  };
 
-    //for handelling input
-    handelchange = (e) => {
-        this.setState({ form: { ...this.state.form, [e.target.name]: e.target.value } });
-    };
+  //for handelling input
+  handelchange = (e) => {
+    this.setState({
+      form: { ...this.state.form, [e.target.name]: e.target.value },
+    });
+  };
 
-    //for submit
-    onsubmit = (e) => {
-        e.preventDefault();
-        if (this.state.form.password !== this.state.form.confirm_password) {
-            this.props.AddAlert("password donot match", "bad password");
+  //for submit
+  onsubmit = (e) => {
+    e.preventDefault();
+    if (this.state.form.password !== this.state.form.confirm_password) {
+      this.props.AddAlert("password donot match", "bad password");
 
-            this.setState({ form: { ...this.state.form, confirm_password: "" } });
-        } else {
-            this.props.register({ ...this.state.form });
-            this.setState({ form: { ...this.state.form, username: "", password: "" } });
-        }
-    };
-
-    //onfile upload
-    onFileChange = (e) => {
-        this.setState({
-            form: { ...this.state.form, file: e.target.files[0] },
-            image: URL.createObjectURL(e.target.files[0]),
-        });
-    };
-
-    //for redirect after successful registering
-    componentDidUpdate(props) {
-        if (this.props.isAuthenticated === true) {
-            this.props.history.push("/");
-        }
+      this.setState({ form: { ...this.state.form, confirm_password: "" } });
+    } else {
+      this.props.register({ ...this.state.form });
+      this.setState({
+        form: { ...this.state.form, username: "", password: "" },
+      });
     }
+  };
 
-    render() {
-        return (
-            <div className="div1-register">
-                <div className="Register">
-                    <h1>Register</h1>
-                    <p>Create An account to manage your Events...</p>
-                    <div className="container">
-                        <form onSubmit={this.onsubmit} className="left">
-                            <div>
-                                <label>clubname</label>
-                            </div>
-                            <input type="text" value={this.state.form.username} placeholder="The name" onChange={this.handelchange} name="username" />
-                            <div>
-                                <label>Bio</label>
-                            </div>
-                            <textarea style={{ width: "350px", minHeight: "100px" }} value={this.state.form.bio} onChange={this.handelchange} type="text" placeholder="The Fame" name="bio" />
-                            <div>
-                                <label>Email</label>
-                            </div>
-                            <input type="email" placeholder="Enter Email" value={this.state.form.email} onChange={this.handelchange} name="email" required />
-                            <div>
-                                <label>Password</label>
-                            </div>
-                            <input
-                                type="password"
-                                name="password"
-                                onChange={this.handelchange}
-                                // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                                value={this.state.form.password}
-                                title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
-                                required
-                            />
-                            <div>
-                                <label>Repeat Password</label>
-                            </div>
-                            <input
-                                type="password"
-                                id="reppassword"
-                                name="confirm_password"
-                                onChange={this.handelchange}
-                                // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                                value={this.state.form.confirm_password}
-                                title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
-                                required
-                            />
-                            <input type="submit" value="Register  " />
-                        </form>
-                    </div>
-                </div>
+  //onfile upload
+  onFileChange = (e) => {
+    this.setState({
+      form: { ...this.state.form, file: e.target.files[0] },
+      image: URL.createObjectURL(e.target.files[0]),
+    });
+  };
 
-                <div className="Register right center">
-                    <div>
-                        <img src={this.state.image} />
-                        <input type="file" onChange={this.onFileChange} />
-                        <input type="submit" onClick={this.fileupload} value="Upload " />
-                    </div>
-                    Already have an account?{" "}
-                    <Link to="./login" className="submit">
-                        {" "}
-                        Log In!
-                    </Link>
-                    .
-                </div>
-            </div>
-        );
+  //for redirect after successful registering
+  componentDidUpdate(props) {
+    if (this.props.isAuthenticated === true) {
+      this.props.history.push("/");
     }
+  }
+
+  render() {
+    return (
+      <div className="div1-register">
+        <div className="register">
+          <div className="container">
+            <h1>Register</h1>
+            <p>Create An account to manage your Events...</p>
+            <form onSubmit={this.onsubmit} className="left">
+              <div>
+                <label>clubname</label>
+              </div>
+              <input
+                type="text"
+                value={this.state.form.username}
+                placeholder="Name"
+                onChange={this.handelchange}
+                name="username"
+                required
+              />
+              <div>
+                <label>Bio</label>
+              </div>
+              <textarea
+                style={{ minHeight: "100px" }}
+                value={this.state.form.bio}
+                onChange={this.handelchange}
+                type="text"
+                placeholder="The Fame"
+                name="bio"
+                required
+              />
+              <div>
+                <label>Email</label>
+              </div>
+              <input
+                type="email"
+                placeholder="Email"
+                value={this.state.form.email}
+                onChange={this.handelchange}
+                name="email"
+                required
+              />
+              <div>
+                <label>Password</label>
+              </div>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={this.handelchange}
+                // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                value={this.state.form.password}
+                title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+                required
+              />
+              <div>
+                <label>Repeat Password</label>
+              </div>
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                id="reppassword"
+                name="confirm_password"
+                onChange={this.handelchange}
+                // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                value={this.state.form.confirm_password}
+                title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+                required
+              />
+              <img src={this.state.image} />
+              <input type="file" onChange={this.onFileChange} />
+              <input type="submit" value="Register" className="submit-button" />
+              <div className="have-account">
+                Already have an account?&nbsp;&nbsp;
+                <Link to="./login" className="login-link">
+                  Log In!
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 Register.proptype = {
-    isAuthenticated: proptype.bool.isRequired,
+  isAuthenticated: proptype.bool.isRequired,
 };
 const mapstatetoprops = (state) => ({
-    isAuthenticated: state.auth.isAuthenticated,
+  isAuthenticated: state.auth.isAuthenticated,
 });
 
 export default connect(mapstatetoprops, { AddAlert, register })(Register);
